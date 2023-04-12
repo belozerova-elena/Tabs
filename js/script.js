5143226633b13dc3c0cbf55eba809fd030f5193e
belozerova-elena <lena.belozerova20@gmail.com>
2023-04-12 08:54:21 +0500

    add js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelector('.tabs');
+const btns = document.querySelectorAll('.btns__tab');
+const articles = document.querySelectorAll('.content');
+
+tabs.addEventListener('click', function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach(function (btn) {
+      btn.classList.remove('active');
+    });
+    e.target.classList.add('active');
+    articles.forEach(function (article) {
+      article.classList.remove('active');
+    });
+    const element = document.getElementById(id);
+    element.classList.add('active');
+  }
+});
